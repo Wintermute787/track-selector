9b3ee4cfe494a2d097a93d1ee77acb366ecef2e0
Joe Barnes <joebarnes333@gmail.com>
2019-03-29 15:58:49 -0700

    add background and positioning to page

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ var php = 0;
 
 $(function () {
 
+
+
     $("#formOne").submit(function (event) {
         event.preventDefault();
 
@@ -133,4 +135,25 @@ $(function () {
 
         event.preventDefault();
     });
+
+    $(window).load(function(){
+        var theWindow         = $(window),
+        $bg                   = $("#bg"),
+        aspectRatio           = $bg.width() / $bg.height();
+
+        function resizeBg() {
+            if((theWindow.width() / theWindow.height()) < aspectRatio) {
+                $bg
+                    .removeClass()
+                    .addClass('bgheight');
+            }else {
+                $bg
+                    .removeClass()
+                    .addClass('bgwidth');
+            }
+        }
+        theWindow.resize(resizeBg).trigger("resize");
+    });
+
+   
 });
